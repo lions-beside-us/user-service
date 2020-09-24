31fefd50e6db338bdc9603e1843064a08386c383
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-09-24 10:33:58 -0700

    user schema and save method created

--- db/index.js
+++ db/index.js
@@ -11,3 +11,37 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('mongodb connected!')
 });
+
+const userSchema = new mongoose.Schema({
+  user_id: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  user_name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String
+  },
+  profile_image_url: {
+    type: String
+  }
+});
+
+let User = mongoose.model('User', userSchema);
+
+let saveUser = (user) => {
+  let newUser = new User({
+    user_id:           user.user_id,
+    user_name:         user.user_name,
+    location:          user.location,
+    profile_image_url: user.profile_image_url
+  });
+
+  newUser.save((err, newUser) => {
+    if (err) return console.error('save error: ', err);
+  })
+
+}
