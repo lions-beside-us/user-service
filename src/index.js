65c316c2fbc119bf63c14f2d79de68836fc65c2a
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-03 13:45:22 -0700

    setup babel, webpack and react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+        <div>Users will be here.. eventually WOOP WOOP</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#users"));
