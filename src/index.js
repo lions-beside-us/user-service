90bdd228ff91fffb10552583eb13b436baa9991b
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-04 13:48:43 -0700

    change mode= based off Nicholas' PR feedback

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const App = () => {
   return (
     <div>
-        <div>Users will be here.. eventually WOOP WOOP</div>
+        <div>Users will be here.. eventually WOOP WOOP (test mode config)</div>
     </div>
   );
 };
